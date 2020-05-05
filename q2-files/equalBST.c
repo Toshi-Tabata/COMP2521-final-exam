baa8b12b33342b10db3dd9f91916d698b02dfef5
Toshi-Tabata <tabata.of.t@gmail.com>
2020-05-05 10:03:51 +1000

    completed equalBST

--- q2-files/equalBST.c
+++ q2-files/equalBST.c
@@ -17,12 +17,19 @@
     - DO NOT add a "main" function to this file. 
 */
 
+
+
 int equalBST(BSTree t1, BSTree t2) {
-	// TODO
-	// Implement this function
-	
-	
-	// change the return value below!
-	return 0;
+    // If t1 and t2 are NULL, return true
+    if (t1 == NULL && t2 == NULL) return 1;
+
+    // If either are NULL (but not both), then we have a mismatch
+    if (t1 == NULL || t2 == NULL) return 0;
+
+    if (key(t1) != key(t2)) return 0;
+
+    // Checks left and right subtrees recursively (DFS manner)
+    // Multiply answers so that only 1 false is required to return false
+    return 1 * equalBST(t1->left, t2->left) * equalBST(t2->right, t2->right);
 }
 
